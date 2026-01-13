49225cbd91bf9557ff7ed5d3e84a633f13bdf03f
Aditya-1406 <gargaditya674@gmail.com>
2026-01-13 16:33:09 +0530

    Add receipe added functionality

--- vegetables/urls.py
+++ vegetables/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getAllReceipe/',views.getAllReceipe,name= 'getAllReceipe')
+    path('getAllReceipe/',views.getAllReceipe,name= 'getAllReceipe'),
+    path('addReceipe/',views.addReceipe,name='addReceipe'),
 ]
--- vegetables/views.py
+++ vegetables/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
@@ -9,3 +9,28 @@ def getAllReceipe(request):
     context = {'receipe' : queryset}
 
     return render(request,'AllReceipe.html',context= context)
+
+def addReceipe(request):
+
+    if request.method == 'POST':
+        receipe_name = request.POST.get('receipe_name')
+        receipe_description = request.POST.get('receipe_description')
+        image = request.FILES.get('image')
+
+        category_name = request.POST.get('category')
+
+        # get or create category object
+        category_obj, created = Category.objects.get_or_create(
+            name=category_name
+        )
+
+        Receipe.objects.create(
+            receipe_name=receipe_name,
+            receipe_description=receipe_description,
+            image=image,
+            category=category_obj
+        )
+
+        return redirect('getAllReceipe')  # use URL name
+
+    return render(request, 'CreateReceipe.html')
